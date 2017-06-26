5d11fc1a144808a354391697c5bb7df5860e7844
jerry <jb7959@naver.com>
2017-06-26 10:26:54 +0900

    다음 로드뷰 연동 모듈 추가 및 KML 오버레이 추가

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var gis = require('./routes/map');
+var kml = require('./routes/kml');
+var daumLoadView = require('./routes/daumLoadView');
 
 var app = express();
+
+app.use(express.static('public'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/gis', gis);
+app.use('/kml', kml);
+app.use('/loadmap', daumLoadView);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
